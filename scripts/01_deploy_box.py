7c260d8ed3f91be740101b4e0ed2883e83524fdf
Suranjan <suranjanbiswas2002@gmail.com>
2021-10-16 21:45:19 +0530

    add: added basic script for deploying a box contract

--- scripts/01_deploy_box.py
+++ scripts/01_deploy_box.py
@@ -1,2 +1,9 @@
+from brownie import Box, network
+from scripts.utils.helpful_scripts import get_account
+
+
 def main():
-    pass
+    account = get_account()
+    print(f"Deploying to network {network.show_active()}")
+    box = Box.deploy({"from": account})
+    print(box.retreive())
