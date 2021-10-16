40cf6b0ed4990a10b25b3dd5c12b444f4e9183c7
Suranjan <suranjanbiswas2002@gmail.com>
2021-10-16 22:00:51 +0530

    add: added initial step for proxies

--- scripts/01_deploy_box.py
+++ scripts/01_deploy_box.py
@@ -1,9 +1,12 @@
-from brownie import Box, network
+from brownie import Box, ProxyAdmin, network
 from scripts.utils.helpful_scripts import get_account
 
 
 def main():
     account = get_account()
     print(f"Deploying to network {network.show_active()}")
-    box = Box.deploy({"from": account})
+    box = Box.deploy({"from": account})  # the starting implementation contract
     print(box.retreive())
+
+    # hooking up an implementation contract to a proxy
+    proxy_admin = ProxyAdmin.deploy({"from": account})
