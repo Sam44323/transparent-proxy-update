f204112f08ab297b1daacecc60df0ebaa2f8b39c
Suranjan <suranjanbiswas2002@gmail.com>
2021-10-17 21:13:06 +0530

    add: added test file for boxv2 upgradews

---
+++ tests/test_box_v2_upgrades.py
@@ -0,0 +1,6 @@
+from scripts.utils.helpful_scripts import encode_function_data, get_account
+from brownie import BoxV2, ProxyAdmin, TransparentUpgradeableProxy, Contract
+
+
+def test_proxy_delegate_calls():
+    pass
