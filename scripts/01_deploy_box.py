98fccd2bcb9887ff2ee8ef56933f5173c47a3067
Suranjan <suranjanbiswas2002@gmail.com>
2021-10-17 14:23:05 +0530

    add: added transparent proxy deployment

--- scripts/01_deploy_box.py
+++ scripts/01_deploy_box.py
@@ -1,5 +1,5 @@
-from brownie import Box, ProxyAdmin, network
-from scripts.utils.helpful_scripts import get_account
+from brownie import Box, ProxyAdmin, TransparentUpgradeableProxy, network
+from scripts.utils.helpful_scripts import encode_function_data, get_account
 
 
 def main():
@@ -11,5 +11,9 @@ def main():
     proxy_admin = ProxyAdmin.deploy({"from": account})
     # hooking up an implementation contract to a proxy
 
-    # encoding the initializer function of the implementation contract
-    initializer = box.store, 1
+    # as of now we are not using any initializer function. See the definition for encode_function_data in the helpful scripts
+    box_encoded_initializer_function = encode_function_data()
+    proxy = TransparentUpgradeableProxy.deploy(
+        box.address, proxy_admin.address, box_encoded_initializer_function, {"from": account, "gas_limit": 1000000})
+
+    print(f"Proxy upload to {proxy}, you can now update to v2!")
