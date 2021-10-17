1774f4acdb5e4d913a9db49e26d7f58378dedfbc
Suranjan <suranjanbiswas2002@gmail.com>
2021-10-17 21:25:19 +0530

    update: updates the script testing for BoxV2 contract checker

--- tests/test_box_v2_upgrades.py
+++ tests/test_box_v2_upgrades.py
@@ -1,6 +1,27 @@
-from scripts.utils.helpful_scripts import encode_function_data, get_account
-from brownie import BoxV2, ProxyAdmin, TransparentUpgradeableProxy, Contract
+from scripts.utils.helpful_scripts import encode_function_data, get_account, upgrade
+from brownie import Box, BoxV2, ProxyAdmin, TransparentUpgradeableProxy, Contract
 
 
 def test_proxy_delegate_calls():
     account = get_account()
+    box = Box.deploy({"from": account})
+    proxy_admin = ProxyAdmin.deploy({"from": account})
+
+    box_encoded_initializer_function = encode_function_data()
+    proxy = TransparentUpgradeableProxy.deploy(
+        box.address, proxy_admin.address, box_encoded_initializer_function, {"from": account})
+
+    proxy_box = Contract.from_abi("Box", proxy.address, Box.abi)
+    assert proxy_box.retrieve({"from": account}) == 0
+    proxy_box.store(1, {"from": account})
+    assert proxy_box.retrieve({"from": account}) == 1
+
+    # asserting the update for implementation contract
+    box_v2 = BoxV2.deploy({"from": account})
+    upgrade_transaction = upgrade(
+        account, proxy, box_v2.address, proxy_admin)
+    upgrade_transaction.wait(1)
+    print("Proxy has been updated!")
+    proxy_box = Contract.from_abi("BoxV2", proxy.address, BoxV2.abi)
+    proxy_box.increment({"from": account})
+    assert proxy_box.retreive({"from": account}) == 2
