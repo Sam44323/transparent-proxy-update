ccb650acfd14b0f8ba928045b21f5a2970f454af
Suranjan <suranjanbiswas2002@gmail.com>
2021-10-17 15:58:33 +0530

    add: added networks to config

--- scripts/01_deploy_box.py
+++ scripts/01_deploy_box.py
@@ -14,11 +14,12 @@ def main():
     # as of now we are not using any initializer function. See the definition for encode_function_data in the helpful scripts
     box_encoded_initializer_function = encode_function_data()
     proxy = TransparentUpgradeableProxy.deploy(
-        box.address, proxy_admin.address, box_encoded_initializer_function, {"from": account, "gas_limit": 1000000})
+        box.address, proxy_admin.address, box_encoded_initializer_function, {"from": account})
 
     print(f"Proxy upload to {proxy}, you can now update to v2!")
 
     # assigning the Box ABI to proxy contract so that it can delegate to the box contract once called instead of throwing an error(which is a default usecase when you assign an ABI to a contract that doesn't contain any functions related to the abi)
 
     proxy_box = Contract.from_abi("Box", proxy.address, Box.abi)
+    proxy_box.store(1, {"from": account})
     print(proxy_box.retreive())
