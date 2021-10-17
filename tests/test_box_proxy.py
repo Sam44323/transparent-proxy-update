95ca61d514bb16051649474206462612d70fed95
Suranjan <suranjanbiswas2002@gmail.com>
2021-10-17 21:12:38 +0530

    update: updated the import for box test

--- tests/test_box_proxy.py
+++ tests/test_box_proxy.py
@@ -1,5 +1,5 @@
 from scripts.utils.helpful_scripts import encode_function_data, get_account
-from brownie import Box, BoxV2, ProxyAdmin, TransparentUpgradeableProxy, Contract
+from brownie import Box, ProxyAdmin, TransparentUpgradeableProxy, Contract
 
 
 def test_proxy_delegate_calls():
