a969a78694441b8df7a04fbb831f2822d964c168
Suranjan <suranjanbiswas2002@gmail.com>
2021-10-17 13:43:19 +0530

    update: updated the docs

--- scripts/01_deploy_box.py
+++ scripts/01_deploy_box.py
@@ -5,8 +5,8 @@ from scripts.utils.helpful_scripts import get_account
 def main():
     account = get_account()
     print(f"Deploying to network {network.show_active()}")
-    box = Box.deploy({"from": account})  # the starting implementation contract
+    box = Box.deploy({"from": account})  # the Box implementation contract
     print(box.retreive())
 
-    # hooking up an implementation contract to a proxy
     proxy_admin = ProxyAdmin.deploy({"from": account})
+    # hooking up an implementation contract to a proxy
