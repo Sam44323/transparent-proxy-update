cfe9970bc3033e4afc74813ce05a04dabb5aeab0
Suranjan <suranjanbiswas2002@gmail.com>
2021-10-17 21:03:45 +0530

    add: added test for checking the initial delegation for Box contract using proxy

--- tests/test_box_proxy.py
+++ tests/test_box_proxy.py
@@ -1,5 +1,17 @@
-from scripts.utils.helpful_scripts import get_account
+from scripts.utils.helpful_scripts import encode_function_data, get_account
+from brownie import Box, BoxV2, ProxyAdmin, TransparentUpgradeableProxy, Contract
 
 
 def test_proxy_delegate_calls():
     account = get_account()
+    box = Box.deploy({"from": account})
+    proxy_admin = ProxyAdmin.deploy({"from": account})
+
+    box_encoded_initializer_function = encode_function_data()
+    proxy = TransparentUpgradeableProxy.deploy(
+        box.address, proxy_admin.address, box_encoded_initializer_function, {"from": account})
+
+    proxy_box = Contract.from_abi("Box", proxy.address, Box.abi)
+    assert proxy_box.retrieve({"from": account}) == 0
+    proxy_box.store(1, {"from": account})
+    assert proxy_box.retrieve({"from": account}) == 1
