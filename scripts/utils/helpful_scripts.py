d4efa876abfb9f6df48a3d6b969c7c21b7018fea
Suranjan <suranjanbiswas2002@gmail.com>
2021-10-17 14:01:40 +0530

    add: added an encode function data method for encoding the initializer

--- scripts/utils/helpful_scripts.py
+++ scripts/utils/helpful_scripts.py
@@ -1,4 +1,5 @@
 from brownie import accounts, network, config
+import eth_utils
 
 LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["hardhat",
                                  "development", "ganache", "mainnet-fork"]
@@ -12,3 +13,12 @@ def get_account(index=None, id=None):
     if id:
         return accounts.load(id)
     return accounts.add(config["wallets"]["from_key"])
+
+# encoding the initializer function into bytes so that our proxy smart contract knows what function to call when initiated
+
+
+def encode_function_data(initializer=None, *args):
+    if len(args) == 0 or not initializer:
+        # if no initializer or args is blank, we return an empty hex string
+        return eth_utils.to_bytes(hexstr="0x")
+    return initializer.encode_input(*args)
